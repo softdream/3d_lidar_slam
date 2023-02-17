b003a17806761819e17055eb5200970174c96b5c
daofeng <9779716+du-daofeng@user.noreply.gitee.com>
2023-02-17 21:56:12 +0800

    3d lidar slam

--- feature_extraction/feature_extract.h
+++ feature_extraction/feature_extract.h
@@ -2,7 +2,9 @@
 #define __FEATURE_EXTRACT_H
 
 #include <vector>
-#include "point_cloud.h"
+
+#include <any>
+#include <iostream>
 
 namespace slam
 {
@@ -24,11 +26,27 @@ public:
 	template<typename InputCloudType, typename ...OutputCloudsType>
 	void extractFeaturesFromCloud( const InputCloudType& input_cloud, OutputCloudsType... output_clouds )
 	{
-		if constexpr ( auto ptr = static_cast<DerivedType*>( this ) ) {
+		if ( auto ptr = static_cast<DerivedType*>( this ) ) {
 			ptr->extractFeaturesFromCloud( input_cloud, output_clouds... );
 		}
 	}
-	
+
+protected:
+        template<typename OutputCloudType, typename ...OutputCloudsType>
+        void extractFeaturesFromCloudHelper( OutputCloudType&& ouput_cloud, OutputCloudsType... output_clouds )
+        {
+		output_clouds_ptrs_vec_.push_back( &ouput_cloud );
+
+                extractFeaturesFromCloudHelper( output_clouds... );
+        }
+
+        void extractFeaturesFromCloudHelper()
+        {
+
+        }
+
+protected:
+	std::vector<std::any> output_clouds_ptrs_vec_;
 };
 
 class CornerPlannerFeature : public FeatureBase<CornerPlannerFeature>
@@ -42,22 +60,14 @@ public:
 	template<typename InputCloudType, typename ...OutputCloudsType>
         void extractFeaturesFromCloud( const InputCloudType& input_cloud, OutputCloudsType... output_clouds )
         {
+		static_assert( !(sizeof...( output_clouds ) != 2 ), "The number of arguments is too few !");
+
                 std::cout<<"Corner Planner Feature !"<<std::endl;
 
+		// parse the paramters
                 this->extractFeaturesFromCloudHelper( input_cloud, output_clouds... );
-
-        }
-
-private:
-	template<typename OutputCloudType, typename ...OutputCloudsType>
-        void extractFeaturesFromCloudHelper( OutputCloudType&& ouput_cloud, OutputCloudsType... output_clouds )
-        {
-                extractFeaturesFromCloudHelper( output_clouds... );
-        }
-
-        void extractFeaturesFromCloudHelper()
-        {
-
+	
+		
         }
 
 };
